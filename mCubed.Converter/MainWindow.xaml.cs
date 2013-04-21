f95d068a49c2957526ad304d6ff6c2af4a8aa50b
nickp10 <nickp10@hotmail.com>
2013-04-21 20:38:27 +0000

    Developer: Nick Paddock Bug/Feature #: N/A Description: Creating mCubed.Converter skeleton.

---
+++ mCubed.Converter/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.ComponentModel;
+using System.Windows;
+
+namespace mCubed.Converter
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window, INotifyPropertyChanged
+	{
+		#region Directory
+
+		private string _directory = @"C:\Users\Nick\Downloads\";
+		public string Directory
+		{
+			get { return _directory; }
+			set
+			{
+				if (_directory != value)
+				{
+					_directory = value;
+					OnPropertyChanged("Directory");
+				}
+			}
+		}
+
+		#endregion
+
+		#region INotifyPropertyChanged Members
+
+		protected void OnPropertyChanged(string property)
+		{
+			var handler = PropertyChanged;
+			if (handler != null)
+			{
+				handler(this, new PropertyChangedEventArgs(property));
+			}
+		}
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		#endregion
+
+		public MainWindow()
+		{
+			DataContext = this;
+			InitializeComponent();
+		}
+
+		private void OnConvertClick(object sender, RoutedEventArgs e)
+		{
+
+		}
+	}
+}
